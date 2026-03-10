d5c4d006fdbc9fc4440488196f4054a07d038477
pawandhuriya@outlook.com <pawandhuriya@outlook.com>
2026-03-10 18:03:52 +0530

    Test Commit

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "java-app-bucket-01"
     key = "prod/terraform/tfstate"
     region = "us-east-1"
-    use_lockfile = "terraform-lock"
+    dynamodb_table = "uses_lockfile"
     encrypt = true
   }
 }
