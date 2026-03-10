72a5c364313699bf5171d5a6ce2c723765ce7575
pawandhuriya@outlook.com <pawandhuriya@outlook.com>
2026-03-10 18:18:36 +0530

    Test Commit

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -1,27 +1,36 @@
 resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/16"
-
-    tags ={
-        Name = "main-vpc"
-    }
-  
-}
-
-resource "aws_internet_gateway" "gw" {
-    vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main-vpc"
+  }
 }
 
 resource "aws_subnet" "public_1" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.1.0/24"
-    availability_zone = "us-east-1a"
-  
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = { Name = "public-subnet-1" }
 }
 
 resource "aws_subnet" "public_2" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.2.0/24"
-    availability_zone = "us-east-1b"
-  
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+  tags = { Name = "public-subnet-2" }
 }
 
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.101.0/24"
+  availability_zone = "us-east-1a"
+  tags = { Name = "private-subnet-1" }
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.102.0/24"
+  availability_zone = "us-east-1b"
+  tags = { Name = "private-subnet-2" }
+}
