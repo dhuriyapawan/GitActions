9ff6f7d85e358f2edf12307096c66e70f5e8263c
pawandhuriya@outlook.com <pawandhuriya@outlook.com>
2026-03-10 18:15:16 +0530

    Test Commit

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,13 +1,15 @@
-# outputs.tf
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "java-app-bucket-01-${random_id.bucket_suffix.hex}"
+  
+}
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
 
 output "s3_bucket_name" {
   value       = aws_s3_bucket.my_bucket.bucket
-  description = "The name of the S3 bucket"
-}
-
-output "vpc_id" {
-  value       = aws_vpc.main.id
-  description = "The ID of the main VPC"
+  description = "java-app-bucket-01"
 }
 
 output "ec2_public_ip" {
