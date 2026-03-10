f2170316b85e2f63c4c3e2487ad93779cd4f9508
pawandhuriya@outlook.com <pawandhuriya@outlook.com>
2026-03-10 18:20:36 +0530

    Test Commit

--- Terraform/compute.tf
+++ Terraform/compute.tf
@@ -1,25 +1,31 @@
-resource "aws_launch_template" "app" {
-    name_prefix = "app-template"
-    image_id = "ami-0aaa636894689fa47"
-    instance_type = "t3.micro"
-  
+resource "aws_instance" "web" {
+  ami           = "ami-0aaa636894689fa47" # Amazon Linux 2 in us-east-1
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private_1.id
+  security_groups = [aws_security_group.app_sg.name]
+
+  tags = {
+    Name = "web-instance"
+  }
 }
 
 resource "aws_autoscaling_group" "app_asg" {
-
-  desired_capacity = 2
-  max_size         = 4
-  min_size         = 2
-
-  vpc_zone_identifier = [
-    aws_subnet.private_1.id,
-    aws_subnet.private_2.id
-  ]
-
+  desired_capacity     = 1
+  max_size             = 2
+  min_size             = 1
+  vpc_zone_identifier  = [aws_subnet.private_1.id, aws_subnet.private_2.id]
   launch_template {
-    id      = aws_launch_template.app.id
+    id      = aws_launch_template.web_lt.id
     version = "$Latest"
   }
+}
 
-  target_group_arns = [aws_lb_target_group.app_tg.arn]
+resource "aws_launch_template" "web_lt" {
+  name_prefix   = "web-lt-"
+  image_id      = "ami-0aaa636894689fa47"
+  instance_type = "t2.micro"
+  network_interfaces {
+    security_groups = [aws_security_group.app_sg.id]
+    associate_public_ip_address = false
+  }
 }
