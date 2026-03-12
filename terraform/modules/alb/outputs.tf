ff0151c65573689bb7f386d644811d3b1ce1af45
pawandhuriya@outlook.com <pawandhuriya@outlook.com>
2026-03-12 15:55:27 +0530

    test

--- terraform/modules/alb/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "target_group_arn" {
-  description = "ARN of the target group"
-  value       = aws_lb_target_group.tg.arn
-}
-
-output "alb_dns_name" {
-  description = "DNS name of the ALB"
-  value       = aws_lb.app.dns_name
-}
--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,18 +1,22 @@
-resource "aws_instance" "backend" {
-  count                = length(var.private_subnets)
-  ami                  = var.ami
-  instance_type        = var.instance_type
-  subnet_id            = var.private_subnets[count.index]
-  vpc_security_group_ids = var.security_groups
+resource "aws_launch_template" "java_app" {
+  name_prefix   = "java-app-"
+  image_id      = var.ami_id
+  instance_type = var.instance_type
+  security_group_names = [var.sg_name]
+  
+  user_data = file(var.user_data_file)
+}
 
-  user_data = <<EOF
-#!/bin/bash
-amazon-linux-extras install docker -y
-service docker start
-docker run -d -p 8080:8080 ${var.docker_image}
-EOF
-
-  tags = {
-    Name = "java-backend-${count.index + 1}"
+resource "aws_autoscaling_group" "java_app_asg" {
+  launch_template {
+    id      = aws_launch_template.java_app.id
+    version = "$Latest"
   }
+  vpc_zone_identifier = [var.private_subnet_id]
+  min_size            = var.min_size
+  max_size            = var.max_size
+  desired_capacity    = var.desired_capacity
+  target_group_arns   = [var.target_group_arn]
+  health_check_type   = "EC2"
+  health_check_grace_period = 300
 }
