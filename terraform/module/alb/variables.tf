68830aa3d9457a50a83e7e6fefc7a7f069f477a9
pawandhuriya@outlook.com <pawandhuriya@outlook.com>
2026-03-13 11:22:06 +0530

    Test AWS OIDC

--- terraform/module/alb/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "lb_name" {}
-variable "tg_name" {}
-variable "sg_id" {}
-variable "public_subnets" { type = list(string) }
-variable "vpc_id" {}
--- terraform/module/ec2/output.tf
+++
@@ -1,2 +0,0 @@
-output "target_group_arn" { value = aws_lb_target_group.app_tg.arn }
-output "alb_dns_name" { value = aws_lb.app_lb.dns_name }
